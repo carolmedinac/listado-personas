cb9764fc79f093a7d0cec8794c8f3683922833b5
Carol Medina <carolmedinac14>
2023-01-17 11:27:04 -0600

    Casi Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Persona } from './persona.model';
+import { PersonasService } from './personas.service';
+import { DataService } from './data.service';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'listado-personas';
+export class AppComponent implements OnInit {
+  titulo = 'Listado de Personas';
+  
+  constructor(){}
+  ngOnInit(): void {
+     firebase.initializeApp({
+      apiKey: "AIzaSyCvzIFt__PI5MLjxFrLy68Tpdu15Yv55_I",
+      authDomain: "listado-personas-3950c.firebaseapp.com",
+
+     })
+  }
 }
---
+++ src/app/login/login.service.ts
@@ -0,0 +1,37 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import { Router } from '@angular/router';
+import { Injectable } from '@angular/core';
+
+@Injectable({providedIn: 'any'})
+export class LoginService {
+
+    token: string;
+
+    constructor(private router:Router){}
+
+    login(email: string, password: string) {
+        firebase.auth().signInWithEmailAndPassword(email, password)
+            .then(
+                response => {
+                    console.log('esta es una pruebA ' + response);
+                    firebase.auth().currentUser?.getIdToken().then(
+                        token => {
+                            console.log(token);
+                            this.token = token;
+                            this.router.navigate(['/']);
+
+                        }
+                    )
+                }
+            )
+         
+    }
+
+    getIdToken() {
+        return this.token;
+    }
+
+
+}
